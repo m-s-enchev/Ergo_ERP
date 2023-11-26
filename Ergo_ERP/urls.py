98b46c4fdf835282d39edd7e2dcaa09a4729d5de
Momchil Enchev <m.s.enchev@gmail.com>
2023-11-26 16:10:44 +0200

    setup

--- Ergo_ERP/urls.py
+++ Ergo_ERP/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',homepage, te)
+    path('', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
 ]
