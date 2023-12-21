4b60554126d1a7834e3337fd7b6c6af347e3457d
Momchil Enchev <m.s.enchev@gmail.com>
2023-12-21 15:36:47 +0200

    sales app

--- Ergo_ERP/urls.py
+++ Ergo_ERP/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
-    path('settings/', TemplateView.as_view(template_name='settings.html'), name='settings')
+    path('settings/', TemplateView.as_view(template_name='settings.html'), name='settings'),
+    path('sales/', include('Ergo_ERP.sales.urls')),
 ]
