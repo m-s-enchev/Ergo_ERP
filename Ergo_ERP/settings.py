9a37dcaf369d82e69a89ec67b4bb164dd954702b
Momchil Enchev <m.s.enchev@gmail.com>
2023-12-25 23:12:10 +0200

    setup product app

--- Ergo_ERP/settings.py
+++ Ergo_ERP/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Ergo_ERP.sales'
+    'Ergo_ERP.sales',
+    'Ergo_ERP.products',
 ]
 
 MIDDLEWARE = [
