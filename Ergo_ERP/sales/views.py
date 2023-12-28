b4305341466cf00962fc788c11817407504ac0e4
Momchil Enchev <m.s.enchev@gmail.com>
2023-12-28 14:47:10 +0200

    new form view

--- Ergo_ERP/sales/views.py
+++ Ergo_ERP/sales/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import FormView
 
-from Ergo_ERP.sales.forms import SalesDocumentForm
+from Ergo_ERP.sales.forms import SalesDocumentForm, SoldProductsFormSet
 
 
 # Create your views here.
@@ -10,3 +11,29 @@ class SalesDocumentView(FormView):
     template_name = "sales/sale.html"
     form_class = SalesDocumentForm
     success_url = "/"
+
+
+def sales_document(request):
+    if request.method == 'POST':
+        sales_document_form = SalesDocumentForm(request.POST)
+        sold_products_formset = SoldProductsFormSet(request.POST, prefix='sold_products')
+
+        if sales_document_form.is_valid() and sold_products_formset.is_valid():
+            sales_document = sales_document_form.save()
+
+            for form in sold_products_formset:
+                if form.cleaned_data:
+                    sold_product = form.save(commit=False)
+                    sold_product.sales_document = sales_document
+                    sold_product.save()
+
+            return redirect(reverse('create_sales_document'))
+    else:
+        sales_document_form = SalesDocumentForm()
+        sold_products_formset = SoldProductsFormSet(prefix='sold_products')
+    context = {
+        'sales_document_form': sales_document_form,
+        'sold_products_formset': sold_products_formset
+    }
+
+    return render(request, 'sales/sale.html', context)
