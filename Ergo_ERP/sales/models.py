f5c69c6851dc458297dd500cd1258f325e1a4f91
Momchil Enchev <m.s.enchev@gmail.com>
2023-12-29 15:45:44 +0200

    optimize field names

--- Ergo_ERP/sales/models.py
+++ Ergo_ERP/sales/models.py
@@ -20,30 +20,31 @@ class SalesDocument(models.Model):
     seller_address = models.CharField(max_length=200, verbose_name='Address')
     seller_accountable_person = models.CharField(max_length=100, verbose_name='accountable person')
     seller_iban = models.CharField(max_length=34, verbose_name='IBAN')
-    seller_representative = models.CharField(max_length=100)
+    seller_representative = models.CharField(null=True, blank=True, max_length=100)
     buyer_name = models.CharField(max_length=100, verbose_name='Buyer')
     buyer_identification_number = models.CharField(max_length=20, verbose_name='Identification number')
     buyer_address = models.CharField(max_length=200, verbose_name='Address')
     buyer_accountable_person = models.CharField(max_length=100, verbose_name='accountable person')
     buyer_representative = models.CharField(max_length=100, verbose_name='representative')
-    sale_total_before_tax = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Subtotal')
-    sale_total_tax = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='VAT')
-    sale_total_final = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='TOTAL')
-    payment_method = models.CharField(max_length=30)
-    due_date = models.DateField()
+
+    sale_total_before_tax = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=10, verbose_name='Subtotal')
+    sale_total_tax = models.DecimalField(null=True,  blank=True,decimal_places=2, max_digits=10, verbose_name='VAT')
+    sale_total_final = models.DecimalField(null=True,  blank=True,decimal_places=2, max_digits=10, verbose_name='TOTAL')
+    payment_method = models.CharField(null=True, blank=True, max_length=30)
+    due_date = models.DateField(null=True, blank=True,)
 
 
 class SoldProducts (models.Model):
-    product = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200)
     product_quantity = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Qty')
     product_price_before_tax = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Price before VAT')
     product_price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Price')
     product_discount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Discount')
     product_total_before_tax = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Amount before VAT')
     product_total = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Amount')
-    product_lot_number = models.CharField(max_length=100, verbose_name='LOT')
-    product_exp_date = models.DateField(verbose_name='Exp. date')
-    sales_document = models.ForeignKey(SalesDocument, on_delete=models.CASCADE)
+    product_lot_number = models.CharField(max_length=100, verbose_name='LOT', null=True, blank=True,)
+    product_exp_date = models.DateField(verbose_name='Exp. date', null=True,  blank=True,)
+    sales_document_in_which_sold = models.ForeignKey(SalesDocument, on_delete=models.CASCADE)
 
 
 
