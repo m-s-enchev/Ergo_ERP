f8204d6d24b34c70c4dee0980fd133d1fad45352
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-01 22:29:25 +0200

    add new row with button fixed nummerators

--- static/js/sale_document.js
+++ static/js/sale_document.js
@@ -1,5 +1,33 @@
-let empty_row = document.getElementById('empty_row').value
-console.log(empty_row)
+let productForm = document.querySelectorAll(".product-form");
+let container = document.querySelector("#sold-products");
+let addButton = document.querySelector("#add-row");
+let totalForms = document.querySelector("#id_sold_products-TOTAL_FORMS");
+
+let formNum = productForm.length-1;
+
+addButton.addEventListener('click', addForm);
+
+function addForm(e) {
+    e.preventDefault()
+
+    let newForm = productForm[0].cloneNode(true);
+    let formRegex = /sold_products-0-/g;
+
+    formNum++
+    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `sold_products-${formNum}-`);
+    container.insertBefore(newForm, addButton);
+
+    totalForms.setAttribute('value', `${formNum+1}`);
+}
+
+
+
+
+
+
+
+
+
 
 
 
