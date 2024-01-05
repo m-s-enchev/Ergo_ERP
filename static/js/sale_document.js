2adc0a7ee8731029076fa50c4386181b459e409a
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-05 23:56:31 +0200

    hide and show columns for invoice

--- static/js/sale_document.js
+++ static/js/sale_document.js
@@ -24,15 +24,47 @@ function addForm(e) {
 }
 
 
+// Hide and show different columns depending on weather it's an invoice or not
 
 let invoiceCheckbox = document.getElementById('invoice-checkbox');
 let invoiceMainHeading = document.querySelector('label[for="invoice-checkbox"]');
+let sellerAndBuyer = document.getElementById('seller-and-buyer');
+let productsTable = document.querySelector('#sold-products table');
 
 invoiceCheckbox.addEventListener('change', displayHandler);
 
+function tableColumnHide(table, priceColumnIndex, amountColumnIndex) {
+    let rows = table.rows
+    for (let i = 0; i < rows.length; i++) {
+      let cells = rows[i].cells;
+      cells[priceColumnIndex].style.display = 'none';
+      cells[amountColumnIndex].style.display = 'none';
+    }
+}
+
+function tableColumnShow(table, priceColumnIndex, amountColumnIndex) {
+    let rows = table.rows
+    for (let i = 0; i < rows.length; i++) {
+      let cells = rows[i].cells;
+      cells[priceColumnIndex].style.display = 'table-cell';
+      cells[amountColumnIndex].style.display = 'table-cell';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    tableColumnHide(productsTable, 5, 8)})
+
 function displayHandler () {
     if (invoiceCheckbox.checked) {
-        invoiceMainHeading.style.fontSize = '2vw';
+        invoiceMainHeading.style.fontWeight = 'bold';
+        sellerAndBuyer.style.display = 'flex';
+        tableColumnShow(productsTable,5,8)
+        tableColumnHide(productsTable,6,9)
+    } else {
+        invoiceMainHeading.style.fontWeight = 'normal';
+        sellerAndBuyer.style.display = 'none';
+        tableColumnShow(productsTable,6,9)
+        tableColumnHide(productsTable,5,8)
     }
 }
 
