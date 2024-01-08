e65f16997e9a787dea33739ac82766e148013e43
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-08 12:43:32 +0200

    fix setting display, incorporate new model

--- Ergo_ERP/sales/views.py
+++ Ergo_ERP/sales/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import FormView
 
-from Ergo_ERP.sales.forms import SalesDocumentForm, SoldProductsFormSet
+from Ergo_ERP.sales.forms import SalesDocumentForm, SoldProductsFormSet, InvoiceDataFormSet
 from django.core import serializers
 
 
@@ -15,27 +15,64 @@ from django.core import serializers
 
 
 def sales_document(request):
-
     if request.method == 'POST':
         sales_document_form = SalesDocumentForm(request.POST)
         sold_products_formset = SoldProductsFormSet(request.POST, prefix='sold_products')
+        invoice_data_formset = InvoiceDataFormSet(request.POST, prefix='invoice_data')
 
-        if sales_document_form.is_valid() and sold_products_formset.is_valid():
+        if sales_document_form.is_valid() and sold_products_formset.is_valid() and invoice_data_formset.is_valid():
             sales_document = sales_document_form.save()
+            sold_products_instances = sold_products_formset.save(commit=False)
 
-            for form in sold_products_formset:
+            for sold_product, form in zip(sold_products_instances, sold_products_formset):
                 if form.cleaned_data:
-                    sold_product = form.save(commit=False)
                     sold_product.sales_document_in_which_sold = sales_document
                     sold_product.save()
+
+            invoice_data_instances = invoice_data_formset.save(commit=False)
+            for invoice_data_instance, form in zip(invoice_data_instances, invoice_data_formset):
+                if form.cleaned_data:
+                    invoice_data_instance.sales_document = sales_document
+                    invoice_data_instance.save()
+
+            sold_products_formset.save_m2m()
+            invoice_data_formset.save_m2m()
 
             return redirect(reverse('sale_new'))
 
     else:
         sales_document_form = SalesDocumentForm()
         sold_products_formset = SoldProductsFormSet(prefix='sold_products')
+        invoice_data_formset = InvoiceDataFormSet(prefix='invoice_data')
+
     context = {
         'sales_document_form': sales_document_form,
         'sold_products_formset': sold_products_formset,
+        'invoice_data_formset': invoice_data_formset,
     }
     return render(request, 'sales/sale.html', context)
+
+    # if request.method == 'POST':
+    #     sales_document_form = SalesDocumentForm(request.POST)
+    #     sold_products_formset = SoldProductsFormSet(request.POST, prefix='sold_products')
+    #
+    #     if sales_document_form.is_valid() and sold_products_formset.is_valid():
+    #         sales_document = sales_document_form.save()
+    #
+    #         for form in sold_products_formset:
+    #             if form.cleaned_data:
+    #                 sold_product = form.save(commit=False)
+    #                 sold_product.sales_document_in_which_sold = sales_document
+    #                 sold_product.save()
+    #
+    #         return redirect(reverse('sale_new'))
+    #
+    # else:
+    #     sales_document_form = SalesDocumentForm()
+    #     sold_products_formset = SoldProductsFormSet(prefix='sold_products')
+    #
+    # context = {
+    #     'sales_document_form': sales_document_form,
+    #     'sold_products_formset': sold_products_formset,
+    # }
+    # return render(request, 'sales/sale.html', context)
