291e8ff10653f6c541ba721db8c4570dd4203a2b
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-13 15:28:39 +0200

    redistribute fields amongst models

--- Ergo_ERP/sales/models.py
+++ Ergo_ERP/sales/models.py
@@ -8,17 +8,20 @@ class SalesDocument(models.Model):
         ('wire_transfer', 'wire transfer'),
         ('add_to_bill', 'add to bill')
     ]
-    date = models.DateField(auto_now_add=True)
+
+    date = models.DateField()
     time = models.TimeField(auto_now_add=True)
     operator = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     warehouse = models.CharField(max_length=100)
 
+    buyer_name = models.CharField(max_length=100, verbose_name='Client')
+
     sale_total_before_tax = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=10, verbose_name='Subtotal')
     sale_total_tax = models.DecimalField(null=True,  blank=True,decimal_places=2, max_digits=10, verbose_name='VAT')
     sale_total_final = models.DecimalField(null=True,  blank=True,decimal_places=2, max_digits=10, verbose_name='TOTAL')
     payment_method = models.CharField(null=True, blank=True, max_length=30)
-    due_date = models.DateField(null=True, blank=True,)
+    due_date = models.DateField(null=True, blank=True)
 
 
 class SoldProducts (models.Model):
@@ -35,13 +38,29 @@ class SoldProducts (models.Model):
 
 
 class InvoiceData(models.Model):
-    invoice_number = models.IntegerField(blank=True, null=True)
+    invoice_number = models.IntegerField(blank=True, null=True, verbose_name='Number')
+    invoice_date = models.DateField(verbose_name='Date')
+    invoice_due_date = models.DateField(verbose_name='Due date')
     buyer_name = models.CharField(max_length=100, verbose_name='Buyer')
     buyer_identification_number = models.CharField(max_length=20, verbose_name='Identification number')
     buyer_address = models.CharField(max_length=200, verbose_name='Address')
     buyer_accountable_person = models.CharField(max_length=100, verbose_name='accountable person')
     buyer_representative = models.CharField(max_length=100, verbose_name='representative')
     sales_document_for_invoice = models.OneToOneField(SalesDocument, on_delete=models.CASCADE)
+
+
+class InvoicedProducts (models.Model):
+    invoiced_product_name = models.CharField(max_length=200, verbose_name='Name')
+    invoiced_product_quantity = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Qty')
+    invoiced_product_price_before_tax = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Price before VAT', null=True, blank=True)
+    invoiced_product_price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Price', null=True, blank=True)
+    invoiced_product_discount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Discount', null=True, blank=True)
+    invoiced_product_total_before_tax = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Amount before VAT', null=True, blank=True)
+    invoiced_product_total = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Amount', null=True, blank=True)
+    invoiced_product_lot_number = models.CharField(max_length=100, verbose_name='LOT', null=True, blank=True)
+    invoiced_product_exp_date = models.DateField(verbose_name='Exp. date', null=True,  blank=True,)
+    invoice_document_in_which_included = models.ForeignKey(InvoiceData, on_delete=models.CASCADE)
+
 
 # seller_name = models.CharField(max_length=100, verbose_name='Seller')
 # seller_identification_number = models.CharField(max_length=20, verbose_name='Identification number')
