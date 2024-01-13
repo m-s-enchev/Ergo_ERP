8356da14c97c3f1551ad83407d16fb5f1516eb06
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-14 00:13:02 +0200

    fix sales logic, handle all

--- Ergo_ERP/sales/views.py
+++ Ergo_ERP/sales/views.py
@@ -1,44 +1,45 @@
-
-
-from django.forms import forms
+from django.db import transaction
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from Ergo_ERP.sales.forms import SalesDocumentForm, SoldProductsFormSet, InvoiceDataFormSet
 
 
+def sold_products_save(sold_products_formset, sales_document_instance):
+    for sold_products_form in sold_products_formset:
+        if sold_products_form.cleaned_data:
+            sold_products_instance = sold_products_form.save(commit=False)
+            sold_products_instance.sales_document_in_which_sold = sales_document_instance
+            sold_products_instance.save()
+
+
+def invoice_data_save (invoice_data_formset, sales_document_instance):
+    for invoice_data_form in invoice_data_formset:
+        if invoice_data_form.cleaned_data:
+            invoice_data_instance = invoice_data_form.save(commit=False)
+            invoice_data_instance.sales_document_for_invoice = sales_document_instance
+            invoice_data_instance.save()
+
+
 def sales_document(request):
+    sales_document_form = SalesDocumentForm(request.POST or None)
+    sold_products_formset = SoldProductsFormSet(request.POST or None, prefix='sold_products')
+    invoice_data_formset = InvoiceDataFormSet(request.POST or None, prefix='invoice_data')
+
     if request.method == 'POST':
-        sales_document_form = SalesDocumentForm(request.POST)
-        sold_products_formset = SoldProductsFormSet(request.POST, prefix='sold_products')
-        invoice_data_formset = InvoiceDataFormSet(request.POST, prefix='invoice_data')
+        if sales_document_form.is_valid() and sold_products_formset.is_valid():
+            if not sales_document_form.cleaned_data['is_linked_to_invoice']:
+                with transaction.atomic():
+                    sales_document_instance = sales_document_form.save()
+                    sold_products_save(sold_products_formset, sales_document_instance)
+                return redirect(reverse('sale_new'))
 
-        if invoice_data_formset.is_valid():
-            print('valid')
-        else:
-            print('not valid')
-
-        if sales_document_form.is_valid() and sold_products_formset.is_valid() and sold_products_formset.has_changed():
-            sales_document_instance = sales_document_form.save()
-
-            for sold_products_form in sold_products_formset:
-                if sold_products_form.cleaned_data:
-                    sold_products_instance = sold_products_form.save(commit=False)
-                    sold_products_instance.sales_document_in_which_sold = sales_document_instance
-                    sold_products_instance.save()
-            if invoice_data_formset.is_valid() and invoice_data_formset.has_changed():
-                for invoice_data_form in invoice_data_formset:
-                    if invoice_data_form.cleaned_data:
-                        invoice_data_instance = invoice_data_form.save(commit=False)
-                        invoice_data_instance.sales_document_for_invoice = sales_document_instance
-                        invoice_data_instance.save()
-
-            return redirect(reverse('sale_new'))
-
-    else:
-        sales_document_form = SalesDocumentForm()
-        sold_products_formset = SoldProductsFormSet(prefix='sold_products')
-        invoice_data_formset = InvoiceDataFormSet(prefix='invoice_data')
+            elif sales_document_form.cleaned_data['is_linked_to_invoice'] and invoice_data_formset.is_valid():
+                with transaction.atomic():
+                    sales_document_instance = sales_document_form.save()
+                    sold_products_save(sold_products_formset, sales_document_instance)
+                    invoice_data_save(invoice_data_formset, sales_document_instance)
+                return redirect(reverse('sale_new'))
 
     context = {
         'sales_document_form': sales_document_form,
@@ -47,46 +48,4 @@ def sales_document(request):
     }
     return render(request, 'sales/sale.html', context)
 
-#########################################################################
-#
-# def sales_document(request):
-#     if request.method == 'POST':
-#         sales_document_form = SalesDocumentForm(request.POST)
-#         sold_products_formset = SoldProductsFormSet(request.POST, prefix='sold_products')
-#         invoice_data_formset = InvoiceDataFormSet(request.POST, prefix='invoice_data')
-#
-#         # Check if the formset is completely empty
-#         if not all([sold_products_formset, invoice_data_formset]) or \
-#                 not all([form.is_valid() for form in sold_products_formset.forms + invoice_data_formset.forms]):
-#             # If the formset is completely empty, consider the form invalid
-#             sales_document_form.add_error(None, 'At least one product or invoice data is required.')
-#         else:
-#             if sales_document_form.is_valid():
-#                 sales_document_instance = sales_document_form.save()
-#
-#                 for sold_products_form in sold_products_formset:
-#                     if sold_products_form.cleaned_data:
-#                         sold_products_instance = sold_products_form.save(commit=False)
-#                         sold_products_instance.sales_document_in_which_sold = sales_document_instance
-#                         sold_products_instance.save()
-#
-#                 for invoice_data_form in invoice_data_formset:
-#                     if invoice_data_form.cleaned_data:
-#                         invoice_data_instance = invoice_data_form.save(commit=False)
-#                         invoice_data_instance.sales_document_for_invoice = sales_document_instance
-#                         invoice_data_instance.save()
-#
-#                 return redirect(reverse('sale_new'))
-#
-#     else:
-#         sales_document_form = SalesDocumentForm()
-#         sold_products_formset = SoldProductsFormSet(prefix='sold_products', formset=EmptyFormSet)
-#         invoice_data_formset = InvoiceDataFormSet(prefix='invoice_data', formset=EmptyFormSet)
-#
-#     context = {
-#         'sales_document_form': sales_document_form,
-#         'sold_products_formset': sold_products_formset,
-#         'invoice_data_formset': invoice_data_formset,
-#     }
-#     return render(request, 'sales/sale.html', context)
 
