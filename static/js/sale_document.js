5d3e469a108a14b8d0fa1b2b4784b72afc05094b
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-17 14:15:49 +0200

    fixed datepicker

--- static/js/sale_document.js
+++ static/js/sale_document.js
@@ -4,19 +4,15 @@ let productForm = document.querySelectorAll(".product-form");
 let container = document.querySelector("#sold-products tbody");
 let addButton = document.querySelector("#add-row");
 let totalForms = document.querySelector("#id_sold_products-TOTAL_FORMS");
-
 let formNum = productForm.length;
 
 addButton.addEventListener('click', addForm);
 
 function addForm(e) {
     e.preventDefault()
-
     let newForm = productForm[0].cloneNode(true);
     let formRegex = /sold_products-0-/g;
     let numeratorRegex = /(<td class="numerator">)\d+(<\/td>)/g;
-
-
     newForm.innerHTML = newForm.innerHTML.replace(formRegex, `sold_products-${formNum}-`);
     formNum++
     newForm.innerHTML = newForm.innerHTML.replace(numeratorRegex, `<td class="numerator">${formNum}</td>`);
@@ -25,14 +21,15 @@ function addForm(e) {
 }
 
 
-// Toggle columns and fields depending on whether it's an invoice or not
+// Toggle columns and fields depending on whether an invoice is needed
 
 let invoiceCheckbox = document.getElementById('id_is_linked_to_invoice');
 let invoiceMainHeading = document.querySelector('label[for="id_is_linked_to_invoice"]');
+let invoiceData = document.getElementById('invoice-number-and-dates');
+let buyerData = document.getElementById('buyer-data');
 let buyerDataToggleFields = document.getElementById('buyer-data-toggle-fields');
 let invoiceToggledFields = document.getElementById('invoice-toggled-fields');
 let productsTable = document.querySelector('#sold-products table');
-
 
 function tableColumnHide(table, priceColumnIndex, amountColumnIndex) {
     let rows = table.rows
@@ -54,8 +51,8 @@ function tableColumnShow(table, priceColumnIndex, amountColumnIndex) {
 
 document.addEventListener('DOMContentLoaded', function () {
     tableColumnHide(productsTable, 5, 8);
-    invoiceToggledFields.style.display = 'none';
-    buyerDataToggleFields.style.display = 'none';
+    invoiceData.removeChild(invoiceToggledFields);
+    buyerData.removeChild(buyerDataToggleFields);
 });
 
 invoiceCheckbox.addEventListener('change', displayHandler);
@@ -63,17 +60,16 @@ invoiceCheckbox.addEventListener('change', displayHandler);
 function displayHandler () {
     if (invoiceCheckbox.checked) {
         invoiceMainHeading.style.fontWeight = 'bold';
-        invoiceToggledFields.style.display = 'flex';
-        buyerDataToggleFields.style.display = 'flex';
+        invoiceData.appendChild(invoiceToggledFields);
+        initializeDatepicker()
+        buyerData.appendChild(buyerDataToggleFields);
+        initializeDatepicker()
         tableColumnShow(productsTable,5,8)
         tableColumnHide(productsTable,6,9)
-
     } else {
         invoiceMainHeading.style.fontWeight = 'normal';
-        invoiceToggledFields.style.display = 'none';
-        buyerDataToggleFields.style.display = 'none';
-        // invoiceNumberAndDates.removeChild(invoiceToggledFields);
-        // buyerData.removeChild(buyerDataToggleFields);
+        invoiceData.removeChild(invoiceToggledFields);
+        buyerData.removeChild(buyerDataToggleFields);
         tableColumnShow(productsTable,6,9)
         tableColumnHide(productsTable,5,8)
     }
