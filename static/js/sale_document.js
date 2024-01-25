bf6efb69233c105f61041ce70562ccec7de9b954
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-25 14:30:58 +0200

    sales document with reformatted javscript and comments

--- static/js/sale_document.js
+++ static/js/sale_document.js
@@ -11,7 +11,9 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
+/** Handles the adding of a new products row in table, which is a SoldProductsForm instance.
+ * If the product_name field of the last row is filled and focus is shifted to another field,
+ * a new row is added for the next product */
 class ProductFormManager {
     constructor() {
         this.container = document.querySelector("#sold-products tbody");
@@ -22,7 +24,7 @@ class ProductFormManager {
 
     addForm() {
             let newForm = this.productForms[0].cloneNode(true);
-            let formRegex = new RegExp(`sold_products-${this.formNum - 1}-`, 'g');
+            let formRegex = /sold_products-0-/g;
             let numeratorRegex = /(<td class="numerator">)\d+(<\/td>)/g;
             newForm.innerHTML = newForm.innerHTML.replace(formRegex, `sold_products-${this.formNum}-`);
             newForm.innerHTML = newForm.innerHTML.replace(numeratorRegex, `<td class="numerator">${this.formNum + 1}</td>`);
@@ -35,7 +37,7 @@ class ProductFormManager {
     attachBlurEventToLastField() {
         let lastFirstField = document.getElementById(`id_sold_products-${this.formNum - 1}-product_name`);
         let needsRowAfter = true;
-        lastFirstField.addEventListener('focus', function (e) {
+        lastFirstField.addEventListener('focus',  (e) => {
             if (e.target.value) {
                 needsRowAfter = false;
             }
@@ -48,28 +50,33 @@ class ProductFormManager {
     }
 }
 
-
+/** Prevents the default behavior of "Enter" key and instead uses it to switch from product_name
+ * to product_quantity and from there to the next row. The purpose is to make its use intuitive
+ * for the user and speed up product entry.*/
 function setupEnterKeyBehavior() {
     const tableContainer = document.querySelector("#sold-products tbody");
     tableContainer.addEventListener('keydown', function (e) {
         if (e.target.tagName === 'INPUT' && e.key === 'Enter') {
             e.preventDefault();
             const currentId = e.target.id;
-            const match = currentId.match(/\d+/);
-            if (match) {
-                const currentRowIndex = parseInt(match[0], 10);
-                if (currentId.includes('-product_name')) {
-                    const nextInput = document.querySelector(`#id_sold_products-${currentRowIndex}-product_quantity`);
-                    if (nextInput) nextInput.focus();
-                } else {
-                    const nextInput = document.querySelector(`#id_sold_products-${currentRowIndex + 1}-product_name`);
-                    if (nextInput) nextInput.focus();
-                }
+            const integerInId = currentId.match(/\d+/);
+            const currentRowIndex = parseInt(integerInId[0], 10);
+            if (currentId.includes('-product_name')) {
+                const nextInput = document.querySelector(`#id_sold_products-${currentRowIndex}-product_quantity`);
+                if (nextInput) nextInput.focus();
+            } else {
+                const productForms = document.querySelectorAll(".product-form");
+                const productFormsLength = productForms.length;
+                const nextInput = document.querySelector(`#id_sold_products-${productFormsLength-1}-product_name`);
+                if (nextInput) nextInput.focus();
             }
         }
     });
 }
 
+
+/** Adds or removes from the html the fields necessary for an invoice,
+ * depending on whether an invoice will be issued */
 function setupInvoiceToggle() {
     const invoiceCheckbox = document.getElementById('id_is_linked_to_invoice');
     const invoiceMainHeading = document.querySelector('label[for="id_is_linked_to_invoice"]');
@@ -117,127 +124,6 @@ function setupInvoiceToggle() {
 }
 
 
-
-
-//
-// // Add row (products form) at end of products table
-// let productForm = document.querySelectorAll(".product-form");
-// let container = document.querySelector("#sold-products tbody");
-// let totalForms = document.querySelector("#id_sold_products-TOTAL_FORMS");
-// let formNum = productForm.length;
-// function addForm() {
-//     let newForm = productForm[0].cloneNode(true);
-//     let formRegex = /sold_products-0-/g;
-//     let numeratorRegex = /(<td class="numerator">)\d+(<\/td>)/g;
-//     newForm.innerHTML = newForm.innerHTML.replace(formRegex, `sold_products-${formNum}-`);
-//     formNum++
-//     newForm.innerHTML = newForm.innerHTML.replace(numeratorRegex, `<td class="numerator">${formNum}</td>`);
-//     container.appendChild(newForm)
-//     totalForms.setAttribute('value', `${formNum}`);
-//     attachBlurEventToLastField();
-// }
-//
-// function attachBlurEventToLastField() {
-//     let lastFirstField = document.getElementById(`id_sold_products-${formNum - 1}-product_name`);
-//     let needsRowAfter = true;
-//     lastFirstField.addEventListener('focus', function(e) {
-//         if (e.target.value) {
-//             needsRowAfter = false;
-//         }
-//     });
-//     lastFirstField.addEventListener('blur', function(e) {
-//         if (e.target.value && e.target === lastFirstField && needsRowAfter === true) {
-//             addForm();
-//         }
-//     });
-// }
-//
-// attachBlurEventToLastField();
-//
-//
-//
-//
-// //Enter KEy - behavior - should be linked to add form - this event listener should be attached the every new form
-// document.addEventListener('DOMContentLoaded', enterKeyBehavior);
-//
-// function enterKeyBehavior() {
-//     container.addEventListener('keydown', enterKeyHandler);
-//     function enterKeyHandler(e) {
-//         if (e.key === 'Enter' && e.target.tagName === 'INPUT') {
-//             e.preventDefault();
-//             if (e.target.id.includes('-product_name')) {
-//                 let indexRegex = /\d+/;
-//                 let currentRowIndex = parseInt(e.target.id.match(indexRegex),10);
-//                 let nextInput= document.getElementById(`id_sold_products-${currentRowIndex}-product_quantity`);
-//                 nextInput.focus();
-//             } else {
-//                 let nextInput = document.getElementById(`id_sold_products-${formNum - 1}-product_name`);
-//                 if (nextInput) {
-//                     nextInput.focus();
-//                 }
-//             }
-//         }
-//     }
-//
-// }
-//
-//
-//
-//
-//
-//
-// // Toggle columns and fields depending on whether an invoice is needed
-// let invoiceCheckbox = document.getElementById('id_is_linked_to_invoice');
-// let invoiceMainHeading = document.querySelector('label[for="id_is_linked_to_invoice"]');
-// let invoiceData = document.getElementById('invoice-number-and-dates');
-// let buyerData = document.getElementById('buyer-data');
-// let buyerDataToggleFields = document.getElementById('buyer-data-toggle-fields');
-// let invoiceToggledFields = document.getElementById('invoice-toggled-fields');
-// let productsTable = document.querySelector('#sold-products table');
-//
-// function tableColumnHide(table, priceColumnIndex, amountColumnIndex) {
-//     let rows = table.rows
-//     for (let i = 0; i < rows.length; i++) {
-//       let cells = rows[i].cells;
-//       cells[priceColumnIndex].style.display = 'none';
-//       cells[amountColumnIndex].style.display = 'none';
-//     }
-// }
-//
-// function tableColumnShow(table, priceColumnIndex, amountColumnIndex) {
-//     let rows = table.rows
-//     for (let i = 0; i < rows.length; i++) {
-//       let cells = rows[i].cells;
-//       cells[priceColumnIndex].style.display = 'table-cell';
-//       cells[amountColumnIndex].style.display = 'table-cell';
-//     }
-// }
-//
-// document.addEventListener('DOMContentLoaded', function () {
-//     tableColumnHide(productsTable, 5, 8);
-//     invoiceData.removeChild(invoiceToggledFields);
-//     buyerData.removeChild(buyerDataToggleFields);
-// });
-//
-// invoiceCheckbox.addEventListener('change', displayHandler);
-//
-// function displayHandler () {
-//     if (invoiceCheckbox.checked) {
-//         invoiceMainHeading.style.fontWeight = 'bold';
-//         invoiceData.appendChild(invoiceToggledFields);
-//         initializeDatepicker()
-//         buyerData.appendChild(buyerDataToggleFields);
-//         initializeDatepicker()
-//         tableColumnShow(productsTable,5,8)
-//         tableColumnHide(productsTable,6,9)
-//     } else {
-//         invoiceMainHeading.style.fontWeight = 'normal';
-//         invoiceData.removeChild(invoiceToggledFields);
-//         buyerData.removeChild(buyerDataToggleFields);
-//         tableColumnShow(productsTable,6,9)
-//         tableColumnHide(productsTable,5,8)
-//     }
-// }
 
 
 
