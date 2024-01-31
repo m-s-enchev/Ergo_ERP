84f895f7db00f9b5e2c7e1c2601f975139745273
Momchil Enchev <m.s.enchev@gmail.com>
2024-01-31 23:15:29 +0200

    add simple search by request

--- Ergo_ERP/inventory/views.py
+++ Ergo_ERP/inventory/views.py
@@ -9,3 +9,10 @@ class InventoryView(ListView):
     template_name = 'inventory/inventory.html'
     context_object_name = 'inventory_list'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search_query') or ''
+        if search_query:
+            queryset = queryset.filter(product_name__icontains=search_query)
+        return queryset
+
