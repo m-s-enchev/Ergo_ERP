a6c541caae90e98b90c7e09e002c4f53575ee11f
Momchil Enchev <m.s.enchev@gmail.com>
2024-02-04 23:38:30 +0200

    list products view

--- Ergo_ERP/products/views.py
+++ Ergo_ERP/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from Ergo_ERP.products.models import ProductsModel
+
 
 # Create your views here.
+
+class ProductsListView(ListView):
+    model = ProductsModel
+    template_name = 'products/products-list.html'
+    context_object_name = 'products_list'
+
+
