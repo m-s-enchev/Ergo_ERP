7fee54d430d666f80e69ca0d7dbfdf778622e1cf
Momchil Enchev <m.s.enchev@gmail.com>
2024-02-05 13:39:19 +0200

    transfer document forms

---
+++ Ergo_ERP/inventory/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from datetime import date
+from django.forms import inlineformset_factory
+from Ergo_ERP.inventory.models import WarehouseDocument, TransferredProducts
+
+
+class WarehouseDocumentForm(forms.ModelForm):
+    date = forms.DateField(
+        initial=date.today(),
+        input_formats=['%d.%m.%Y'],
+        widget=forms.DateInput(format='%d.%m.%Y', attrs={'class': 'datepicker'})
+    )
+
+    class Meta:
+        model = WarehouseDocument
+        fields = '__all__'
+
+
+class TransferredProductsForm(forms.ModelForm):
+    class Meta:
+        model = TransferredProducts
+        fields = '__all__'
+
+
+TransferredProductsFormSet = inlineformset_factory(
+    WarehouseDocument,
+    TransferredProducts,
+    form=TransferredProductsForm,
+    extra=1,
+    can_delete=False,
+)
