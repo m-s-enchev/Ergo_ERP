ab30a96a4e8c5cd9854400a49dabdad495b4b37a
Momchil Enchev <m.s.enchev@gmail.com>
2024-02-16 15:58:45 +0200

    make getPrice work for retail and purchase price

--- static/js/common.js
+++ static/js/common.js
@@ -1,19 +1,21 @@
-function getProductPrice(index) {
-        const productNameInput = document.getElementById(`id_sold_products-${index}-product_name`);
-        const productPriceInput = document.getElementById(`id_sold_products-${index}-product_price`);
 
-        productNameInput.addEventListener('change', function() {
-            const productName = this.value;
-            fetch(`/common/get-product-price/?product_name=${encodeURIComponent(productName)}`)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    }
-                    throw new Error('Network response was not ok.');
-                })
-                .then(data => {
-                    productPriceInput.value = data.product_price;
-                })
-                .catch(error => console.error('There has been a problem with your fetch operation:', error));
-        });
+function getProductPrice(index, modelType) {
+    const productNameInput = document.getElementById(`id_sold_products-${index}-product_name`);
+    const productPriceInput = document.getElementById(`id_sold_products-${index}-product_price`);
+
+    productNameInput.addEventListener('change', function() {
+        const productName = this.value;
+        // Include the modelType in the fetch URL
+        fetch(`/common/get-product-price/?product_name=${encodeURIComponent(productName)}&model_type=${modelType}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(data => {
+                productPriceInput.value = data.product_price;
+            })
+            .catch(error => console.error('There has been a problem with your fetch operation:', error));
+    });
 }
