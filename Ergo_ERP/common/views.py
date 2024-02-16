ad5879d1266f3834acf3782f45ea8fd0f860aff7
Momchil Enchev <m.s.enchev@gmail.com>
2024-02-16 15:57:01 +0200

    make get_price universal

--- Ergo_ERP/common/views.py
+++ Ergo_ERP/common/views.py
@@ -1,15 +1,27 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 
+from Ergo_ERP.inventory.models import Inventory
 from Ergo_ERP.products.models import ProductsModel
 
 
 def get_product_price(request):
     product_name = request.GET.get('product_name', None)
-    if product_name:
-        product = ProductsModel.objects.filter(product_name=product_name).first()
+    model_type = request.GET.get('model_type', None)
+
+    if product_name and model_type:
+        if model_type == 'product':
+            model = ProductsModel
+            price_field_name = 'product_retail_price'
+        elif model_type == 'inventory':
+            model = Inventory
+            price_field_name = 'product_purchase_price'
+        else:
+            return JsonResponse({'error': 'Invalid model name in request'}, status=400)
+
+        product = model.objects.filter(product_name=product_name).first()
         if product:
-            return JsonResponse({'product_price': product.product_retail_price})
+            return JsonResponse({'product_price': getattr(product, price_field_name, None)})
         else:
             return JsonResponse({'product_price': None})
-    return JsonResponse({'error': 'Invalid request'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
