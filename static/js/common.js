870b7b58d63014ba6739a5883831d56cd41343af
Momchil Enchev <m.s.enchev@gmail.com>
2024-02-20 22:04:42 +0200

    implement rewTotal

--- static/js/common.js
+++ static/js/common.js
@@ -20,7 +20,35 @@ function getProductPrice(index, modelName, formsetPrefix, priceFieldSuffix) {
             })
             .then(data => {
                 productPriceInput.value = data.product_price;
+                updateRowSum(index, formsetPrefix, priceFieldSuffix, 'product_total');
             })
             .catch(error => console.error('There has been a problem with your fetch operation:', error));
     });
 }
+
+
+
+function updateRowSum(index, formsetPrefix, priceFieldSuffix, sumFieldSuffix) {
+    const quantityField = document.getElementById(`${formsetPrefix}-${index}-product_quantity`);
+    const priceField = document.getElementById(`${formsetPrefix}-${index}-${priceFieldSuffix}`);
+    const discountField = document.getElementById(`${formsetPrefix}-${index}-product_discount`);
+    const rowSumField = document.getElementById(`${formsetPrefix}-${index}-${sumFieldSuffix}`);
+    let quantity = parseFloat(quantityField.value);
+    let price = parseFloat(priceField.value);
+    let discount = parseFloat(discountField.value);
+
+    if (discount) {
+        rowSumField.value = quantity * price * (1 - discount / 100);
+    } else {
+        rowSumField.value = quantity * price;
+    }
+}
+
+
+
+function rowTotal(index, formsetPrefix, priceFieldSuffix, sumFieldSuffix) {
+    const rowProductForm = document.querySelector(`.product-form:nth-child(${index+1})`);
+    rowProductForm.addEventListener('input', () =>
+        updateRowSum(index, formsetPrefix, priceFieldSuffix, sumFieldSuffix))
+}
+
