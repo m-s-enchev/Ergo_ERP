0ad9db021d02253eaf952c145b7eb99ec787b470
Momchil Enchev <m.s.enchev@gmail.com>
2024-02-25 15:12:44 +0200

    separate fetch product price and inventory price

--- static/js/common.js
+++ static/js/common.js
@@ -1,18 +1,48 @@
 /** Fetches product price from either Products or Inventory models.
  * In case of Inventory ot matches both by name and lot */
-function getProductPrice(index, modelName, formsetPrefix, priceFieldNoVatSuffix) {
-    const productNameInput = document.getElementById(`${formsetPrefix}-${index}-product_name`);
-    const productLotInput = document.getElementById(`${formsetPrefix}-${index}-product_lot_number`);
-    const productPriceInput = document.getElementById(`${formsetPrefix}-${index}-${priceFieldNoVatSuffix}`);
 
-    productNameInput.addEventListener('change', function() {
+// function getProductPrice(index, modelName, formsetPrefix, priceFieldNoVatSuffix, priceFieldWithVatSuffix=null) {
+//     const productNameInput = document.getElementById(`${formsetPrefix}-${index}-product_name`);
+//     const productLotInput = document.getElementById(`${formsetPrefix}-${index}-product_lot_number`);
+//     const productPriceInput = document.getElementById(`${formsetPrefix}-${index}-${priceFieldNoVatSuffix}`);
+//
+//     productNameInput.addEventListener('change', function() {
+//         const productName = this.value;
+//         let fetchUrl = `/common/get-product-price/?product_name=${encodeURIComponent(productName)}&model_name=${modelName}`;
+//         if (productLotInput) {
+//             fetchUrl += `&product_lot=${encodeURIComponent(productLotInput.value)}`;
+//         }
+//
+//         fetch(fetchUrl)
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.json();
+//                 }
+//                 throw new Error('Network response was not ok.');
+//             })
+//             .then(data => {
+//                 productPriceInput.value = data.product_price;
+//                 rowTotal(index, formsetPrefix, priceFieldNoVatSuffix, 'product_total_before_tax');
+//                 if (priceFieldWithVatSuffix) {
+//                     const productPriceInputWithVat = document.getElementById(`${formsetPrefix}-${index}-${priceFieldWithVatSuffix}`);
+//                     productPriceInputWithVat.value = (data.product_vat*0.01+1)*data.product_price
+//                     rowTotal(index, formsetPrefix, priceFieldWithVatSuffix, 'product_total');
+//                 }
+//
+//             })
+//             .catch(error => console.error('There has been a problem with your fetch operation:', error));
+//     });
+// }
+
+
+function getProductPrice(index, formsetPrefix, priceNoVatSuffix, priceWithVatSuffix, priceType) {
+    const nameInput = document.getElementById(`${formsetPrefix}-${index}-product_name`);
+    const priceNoVatInput = document.getElementById(`${formsetPrefix}-${index}-${priceNoVatSuffix}`);
+    const priceWithVatInput = document.getElementById(`${formsetPrefix}-${index}-${priceWithVatSuffix}`);
+
+    nameInput.addEventListener('change', function() {
         const productName = this.value;
-        let fetchUrl = `/common/get-product-price/?product_name=${encodeURIComponent(productName)}&model_name=${modelName}`;
-        if (productLotInput) {
-            fetchUrl += `&product_lot=${encodeURIComponent(productLotInput.value)}`;
-        }
-
-        fetch(fetchUrl)
+        fetch(`/common/get-product-price/?product_name=${encodeURIComponent(productName)}&price_type=${priceType}`)
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -20,8 +50,10 @@ function getProductPrice(index, modelName, formsetPrefix, priceFieldNoVatSuffix)
                 throw new Error('Network response was not ok.');
             })
             .then(data => {
-                productPriceInput.value = data.product_price;
-                rowTotal(index, formsetPrefix, priceFieldNoVatSuffix, 'product_total');
+                priceNoVatInput.value = data.product_price;
+                rowTotal(index, formsetPrefix, priceNoVatSuffix, 'product_total_before_tax');
+                priceWithVatInput.value = (data.product_vat*0.01+1)*data.product_price
+                rowTotal(index, formsetPrefix, priceWithVatSuffix, 'product_total');
             })
             .catch(error => console.error('There has been a problem with your fetch operation:', error));
     });
@@ -29,28 +61,30 @@ function getProductPrice(index, modelName, formsetPrefix, priceFieldNoVatSuffix)
 
 
 /** Calculates quantity*price*discount for every row */
-function rowTotal(index, formsetPrefix, priceFieldSuffix, sumFieldSuffix) {
+function rowTotal(index, formsetPrefix, priceFieldSuffix, totalFieldSuffix) {
     const quantityField = document.getElementById(`${formsetPrefix}-${index}-product_quantity`);
     const priceField = document.getElementById(`${formsetPrefix}-${index}-${priceFieldSuffix}`);
     const discountField = document.getElementById(`${formsetPrefix}-${index}-product_discount`);
-    const rowSumField = document.getElementById(`${formsetPrefix}-${index}-${sumFieldSuffix}`);
+    const rowTotalField = document.getElementById(`${formsetPrefix}-${index}-${totalFieldSuffix}`);
     let quantity = parseFloat(quantityField.value);
     let price = parseFloat(priceField.value);
     let discount = parseFloat(discountField.value);
 
     if (discount) {
-        rowSumField.value = quantity * price * (1 - discount / 100);
+        rowTotalField.value = quantity * price * (1 - discount*0.01);
+        console.log(rowTotalField.value)
     } else {
-        rowSumField.value = quantity * price;
+        rowTotalField.value = quantity * price;
+        console.log(rowTotalField.value)
     }
 }
 
 
 /** Updates row total */
-function updateRowSum(index, formsetPrefix, priceFieldSuffix, sumFieldSuffix) {
+function updateRowTotal(index, formsetPrefix, priceFieldSuffix, totalFieldSuffix) {
     const rowProductForm = document.querySelector(`.product-form:nth-child(${index+1})`);
     rowProductForm.addEventListener('input', () => {
-        rowTotal(index, formsetPrefix, priceFieldSuffix, sumFieldSuffix)
+        rowTotal(index, formsetPrefix, priceFieldSuffix, totalFieldSuffix)
     })
 }
 
