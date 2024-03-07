5ddd11a0f30eb69e7c9a5b441e53e0747b0d77d0
Momchil Enchev <m.s.enchev@gmail.com>
2024-03-07 13:08:35 +0200

    sepparate receiving and shipping models

--- Ergo_ERP/inventory/models.py
+++ Ergo_ERP/inventory/models.py
@@ -5,6 +5,9 @@ class Department(models.Model):
     name = models.CharField(max_length=30)
     location = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Inventory(models.Model):
     product_name = models.CharField(max_length=100, verbose_name='name')
@@ -20,19 +23,25 @@ class Inventory(models.Model):
 
 
 class WarehouseDocument(models.Model):
-    """
-    For both receiving and sending goods
-    """
     date = models.DateField()
     time = models.TimeField(auto_now_add=True)
-    operator = models.CharField(max_length=100)
+    operator = models.CharField(max_length=100, null=True, blank=True)
+    total_sum = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Total', null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class ReceivingDocument(WarehouseDocument):
     shipping_department = models.CharField(max_length=100)
-    shipping_warehouse = models.CharField(max_length=100)
+    receiving_department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
+    receiving = True
+
+
+class ShippingDocument(WarehouseDocument):
+    shipping_department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
     receiving_department = models.CharField(max_length=100)
-    receiving_warehouse = models.CharField(max_length=100)
-    total_sum = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Total',
-                                    null=True, blank=True, )
-    receiving = models.BooleanField(default=False)
+    receiving = False
 
 
 class TransferredProducts(models.Model):
@@ -42,6 +51,16 @@ class TransferredProducts(models.Model):
     product_exp_date = models.DateField(verbose_name='Exp. date', null=True, blank=True)
     product_purchase_price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Purchase price',
                                                  null=True, blank=True)
-    product_value = models.DecimalField(decimal_places=2, max_digits=10,
-                                        null=True, blank=True)
-    warehouse_document_in_which_included = models.ForeignKey(WarehouseDocument, on_delete=models.CASCADE)
+    product_value = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Amount', null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class ReceivedProducts(TransferredProducts):
+    linked_warehouse_document = models.ForeignKey(ReceivingDocument, on_delete=models.CASCADE)
+
+
+class ShippedProducts(TransferredProducts):
+    linked_warehouse_document = models.ForeignKey(ShippingDocument, on_delete=models.CASCADE)
