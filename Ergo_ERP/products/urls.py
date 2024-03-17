6316874d0be60176de7f5c1acf3ef2c6c158ef9b
Momchil Enchev <m.s.enchev@gmail.com>
2024-03-17 16:27:26 +0200

    create product-create template

--- Ergo_ERP/products/urls.py
+++ Ergo_ERP/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from Ergo_ERP.products.views import ProductsListView
+from Ergo_ERP.products.views import ProductsListView, ProductsCreateView
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='products_list'),
+    path('new/', ProductsCreateView.as_view(), name='products_create'),
 ]
