8de26ee60a8ee943a14c52bcefe4e25f1b0ae36a
Momchil Enchev <m.s.enchev@gmail.com>
2024-03-21 21:23:50 +0200

    set quickdial to plus, add unit in receive goods, add plus button to header

--- Ergo_ERP/inventory/models.py
+++ Ergo_ERP/inventory/models.py
@@ -47,6 +47,7 @@ class ShippingDocument(WarehouseDocument):
 class TransferredProducts(models.Model):
     product_name = models.CharField(max_length=200, verbose_name='Name')
     product_quantity = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Qty')
+    product_unit = models.CharField(max_length=20, verbose_name='Unit')
     product_lot_number = models.CharField(max_length=100, verbose_name='LOT', null=True, blank=True)
     product_exp_date = models.DateField(verbose_name='Exp. date', null=True, blank=True)
     product_purchase_price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Purchase price',
