d4a082aa6b76046036243408008f0c6ae73c727a
Momchil Enchev <m.s.enchev@gmail.com>
2024-04-14 18:37:20 +0300

    adapt initial to receiving

--- static/js/warehouse_receiving.js
+++ static/js/warehouse_receiving.js
@@ -1,15 +1,18 @@
-let productNamesDict = {};
+
+
+let productNamesDictAll= {};
 
 function initialReceiveProductRowFunctions () {
     const productForms = document.querySelectorAll(".product-form");
     let numberOfRows = productForms.length;
     fetchProductsAll().then(() => {
         for (let index = 0; index < numberOfRows; index++) {
-            multicolumnDropdown(`#id_transferred_products-${index}-product_name`);
-            // getProductPrice(index, "id_transferred_products", "product_purchase_price", "product_price", "product_retail_price");
+            multicolumnDropdown(`#id_transferred_products-${index}-product_name`, productNamesDictAll);
+            get_purchase_price(index, "id_transferred_products", "product_purchase_price");
             updateRowTotal(index, "id_transferred_products", "product_purchase_price", "product_total_before_tax");
         }
     });
 }
-
-id_transferred_products-0-product_name
+document.addEventListener('DOMContentLoaded', function () {
+    initialReceiveProductRowFunctions();
+});
