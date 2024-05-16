5a7dfd00c072f7bdb9d2f0bee40990d61e24da1c
Momchil Enchev <m.s.enchev@gmail.com>
2024-05-16 13:41:41 +0300

    create suppliers view, correct css, modify clients list

--- Ergo_ERP/suppliers/views.py
+++ Ergo_ERP/suppliers/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from Ergo_ERP.suppliers.models import Suppliers
+
+
+class SuppliersList(ListView):
+    model = Suppliers
+    template_name = 'suppliers/suppliers-list.html'
+    context_object_name = 'suppliers_list'
+    extra_context = {'template_verbose_name': 'Suppliers'}
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search_query') or ''
+        if search_query:
+            queryset = queryset.filter(supplier_name__icontains=search_query)
+        return queryset
