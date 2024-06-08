575c4ad48efb3117402d3aa8463ffe7716d49220
Momchil Enchev <m.s.enchev@gmail.com>
2024-06-08 23:41:04 +0300

    Added tests for receive_in_inventory and subtract from inventory

--- Ergo_ERP/inventory/tests.py
+++ Ergo_ERP/inventory/tests.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.forms import Form
 from unittest.mock import MagicMock
-from .models import Inventory, Department
-from .helper_functions import check_inventory
+from .models import Inventory, Department, ReceivingDocument, ReceivedProducts, ShippingDocument, ShippedProducts
+from .helper_functions import check_inventory, create_inventory_instance, receive_in_inventory, subtract_from_inventory, \
+    update_inventory
+
 
 # -------------------- TEST Helper Functions-----------------------
 
@@ -102,7 +106,242 @@ class CheckInventoryTest(TestCase):
         second_product_form.add_error.assert_called_once_with('product_quantity', "Insufficient quantity !")
 
 
+class CreateInventoryInstanceTest(TestCase):
+    def setUp(self):
+        self.department_receive = Department.objects.create(name='Store', location='Sofia')
+        self.department_ship = Department.objects.create(name='Warehouse', location='Plovdiv')
+        self.user = User.objects.create(username='test_user', password='test_password')
+        self.receiving_document = ReceivingDocument.objects.create(
+            date='2025-05-05',
+            time='22:19:55',
+            total_sum=55.44,
+            operator=self.user,
+            shipping_department=self.department_ship,
+            receiving_department=self.department_receive,
+        )
+
+        self.product_instance = ReceivedProducts.objects.create(
+            product_name='Bucket',
+            product_quantity=10,
+            product_unit='pieces',
+            product_purchase_price=10,
+            product_total=100,
+            department=self.department_receive,
+            linked_warehouse_document=self.receiving_document
+        )
+
+    def test_creates_new_inventory_instance_successfully(self):
+        create_inventory_instance(self.product_instance)
+        self.assertTrue(Inventory.objects.all().count(), 1)
+
+
+class ReceiveInInventoryTest(TestCase):
+    def setUp(self):
+        self.matching_name = 'Bucket'
+        self.matching_lot_number = 'L123'
+        self.department_ship = Department.objects.create(name='Warehouse', location='Plovdiv')
+        self.department_receive = Department.objects.create(name='Store', location='Sofia')
+        self.user = User.objects.create(username='test_user', password='test_password')
+
+        self.receiving_document = ReceivingDocument.objects.create(
+            date='2025-04-05',
+            time='22:19:56',
+            total_sum=55.30,
+            operator=self.user,
+            shipping_department=self.department_ship,
+            receiving_department=self.department_receive,
+        )
+        self.product_instance = ReceivedProducts.objects.create(
+            product_name=self.matching_name,
+            product_quantity=10,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            product_purchase_price=10,
+            product_total=100,
+            department=self.department_receive,
+            linked_warehouse_document=self.receiving_document
+        )
+
+    def test_receive_in_inventory_existing_product_and_lot(self):
+        self.matching_inventory_instance = Inventory.objects.create(
+            product_name=self.matching_name,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            department=self.department_receive,
+            product_quantity=5,
+            product_purchase_price=2,
+            product_total=10
+        )
+        receive_in_inventory(self.matching_inventory_instance, self.product_instance)
+        self.assertEqual(self.matching_inventory_instance.product_quantity, 15)
+        self.assertEqual(self.matching_inventory_instance.product_total, 110)
+        self.assertEqual(
+            self.matching_inventory_instance.purchase_price,
+            (self.matching_inventory_instance.product_total/self.matching_inventory_instance.product_quantity)
+        )
+
+    def test_receive_in_inventory_create_new_instance(self):
+        self.product_instance.product_name = 'New product'
+        receive_in_inventory(None, self.product_instance)
+        self.assertEqual(Inventory.objects.all().count(), 1)
+        self.assertEqual(Inventory.objects.first().product_name, 'New product')
 
 
 
+class SubtractFromInventoryTest(TestCase):
+    def setUp(self):
+        self.matching_name = 'Socket'
+        self.matching_lot_number = 'L123'
+        self.department_ship = Department.objects.create(name='Warehouse', location='Plovdiv')
+        self.department_receive = Department.objects.create(name='Store', location='Sofia')
+        self.user = User.objects.create(username='test_user', password='test_password')
+        self.matching_inventory_instance = Inventory.objects.create(
+            product_name=self.matching_name,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            department=self.department_receive,
+            product_quantity=50,
+            product_purchase_price=5,
+            product_total=250
+        )
+        self.shipping_document = ShippingDocument.objects.create(
+            date='2024-04-05',
+            time='22:19:56',
+            total_sum=55.30,
+            operator=self.user,
+            shipping_department=self.department_ship,
+            receiving_department=self.department_receive,
+        )
+        self.product_instance = ShippedProducts.objects.create(
+            product_name=self.matching_name,
+            product_quantity=10,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            product_purchase_price=5,
+            product_total=50,
+            department=self.department_ship,
+            linked_warehouse_document=self.shipping_document
+        )
+
+    def test_ship_from_inventory_with_enough_quantity(self):
+        subtract_from_inventory(self.matching_inventory_instance, self.product_instance)
+        self.assertEqual(self.matching_inventory_instance.product_quantity, 40)
+        self.assertEqual(self.matching_inventory_instance.product_total, 200)
+        self.assertEqual(
+            self.matching_inventory_instance.product_purchase_price,
+            (self.matching_inventory_instance.product_total/self.matching_inventory_instance.product_quantity)
+        )
+
+    def test_subtract_from_inventory_validation_error(self):
+        self.matching_inventory_instance.product_quantity = 5
+        with self.assertRaises(ValidationError) as context:
+            subtract_from_inventory(self.matching_inventory_instance, self.product_instance)
+        self.assertEqual(
+            str(context.exception),
+            "['There is not enough of product Socket with lot L123.']"
+        )
+
+
+class UpdateInventoryTest(TestCase):
+    def setUp(self):
+        self.matching_name = 'Mop'
+        self.matching_lot_number = 'L123'
+        self.department_ship = Department.objects.create(name='Warehouse', location='Plovdiv')
+        self.department_receive = Department.objects.create(name='Store', location='Sofia')
+        self.user = User.objects.create(username='test_user', password='test_password')
+        self.receiving_document = ReceivingDocument.objects.create(
+            date='2025-04-05',
+            time='22:19:56',
+            total_sum=55.20,
+            operator=self.user,
+            shipping_department=self.department_ship,
+            receiving_department=self.department_receive,
+        )
+        self.product_instance_received_1 = ReceivedProducts.objects.create(
+            product_name=self.matching_name,
+            product_quantity=50,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            product_purchase_price=5,
+            product_total=250,
+            department=self.department_receive,
+            linked_warehouse_document=self.receiving_document
+        )
+
+        self.product_instance_received_2 = ReceivedProducts.objects.create(
+            product_name='Different name',
+            product_quantity=10,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            product_purchase_price=3,
+            product_total=30,
+            department=self.department_receive,
+            linked_warehouse_document=self.receiving_document
+        )
+
+        self.shipping_document = ShippingDocument.objects.create(
+            date='2024-04-05',
+            time='22:18:56',
+            total_sum=55.30,
+            operator=self.user,
+            shipping_department=self.department_ship,
+            receiving_department=self.department_receive,
+        )
+
+        self.product_instance_shipped_1 = ShippedProducts.objects.create(
+            product_name=self.matching_name,
+            product_quantity=4,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            product_purchase_price=5,
+            product_total=20,
+            department=self.department_ship,
+            linked_warehouse_document=self.shipping_document
+        )
+
+        self.product_instance_shipped_2 = ShippedProducts.objects.create(
+            product_name='New product',
+            product_quantity=5,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            product_purchase_price=4,
+            product_total=20,
+            department=self.department_ship,
+            linked_warehouse_document=self.shipping_document
+        )
+
+        self.matching_inventory_instance = Inventory.objects.create(
+            product_name=self.matching_name,
+            product_unit='pieces',
+            product_lot_number=self.matching_lot_number,
+            department=self.department_receive,
+            product_quantity=10,
+            product_purchase_price=5,
+            product_total=50
+        )
+
+    def test_receiving_two_products_one_matches(self):
+        self.product_instances_receiving = [self.product_instance_received_1, self.product_instance_received_2]
+        update_inventory(self.product_instances_receiving, True, self.department_receive)
+        self.assertEqual(Inventory.objects.all().count(), 2)
+        self.assertEqual(Inventory.objects.first().product_quantity, 60)
+        self.assertEqual(Inventory.objects.first().product_total, 300)
+        self.assertEqual(Inventory.objects.first().product_purchase_price, 5.00)
+
+    def test_receiving_two_products_no_matches(self):
+        self.product_instance_received_1.product_name = 'Canu'
+        self.product_instances_receiving = [self.product_instance_received_1, self.product_instance_received_2]
+        update_inventory(self.product_instances_receiving, True, self.department_receive)
+        self.assertEqual(Inventory.objects.all().count(), 3)
+
+    def test_shipping_a_product(self):
+        self.product_instances_shipping = [self.product_instance_shipped_1]
+        print(self.product_instance_shipped_1.product_quantity)
+        print(Inventory.objects.first().product_quantity)
+        Inventory.objects.first().department = self.department_ship
+        update_inventory(self.product_instances_shipping, False, self.department_ship)
+        self.assertEqual(Inventory.objects.all().count(), 1)
+        self.assertEqual(Inventory.objects.first().product_quantity, 6)
+        self.assertEqual(Inventory.objects.first().product_total, 30)
+        self.assertEqual(Inventory.objects.first().product_purchase_price, 5.00)
 
